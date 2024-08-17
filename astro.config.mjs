57e55375a3b38e17312ebbe88d855e13f634d849
Sami Fouad <sfouad@gmail.com>
2024-08-17 16:00:04 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import icon from "astro-icon";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from "@astrojs/tailwind"
+import svelte from "@astrojs/svelte"
+import icon from "astro-icon"
+import react from "@astrojs/react"
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelServerless(),
   site: 'https://samifouad.com',
   integrations: [mdx(), sitemap(), tailwind(), svelte(), icon(), react()],
   experimental: {
