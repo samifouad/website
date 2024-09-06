512cec4f13c15b87f77eeb79ad5f441ff291b1a5
Sami Fouad <sfouad@gmail.com>
2024-09-05 23:16:43 -0600

    small tweak

--- src/components/Score.svelte
+++ src/components/Score.svelte
@@ -1,11 +1,20 @@
 <script>
   export let value = 75; // Default score
   export let size = 120; // Default size
+
+  let bgcolor
   
   const getColor = (value) => {
-    if (value >= 89) return '#00cc66';
-    if (value >= 49) return '#ffaa33';
-    return '#ff4848';
+    if (value >= 89) {
+      bgcolor = '#e5faef'
+      return '#00cc66'
+    } else if (value >= 49) {
+      bgcolor = '#fff6ea'
+      return '#ffaa33';
+    } else {
+      bgcolor = '#ffeaea'
+      return '#ff4848';
+    }
   };
 
   // Calculate the radius and circumference based on the size
@@ -29,9 +38,9 @@
     cx="{size / 2}"
     cy="{size / 2}"
     r="{radius}"
-    fill="none"
+    fill="{bgcolor}"
     stroke="#ccc"
-    stroke-width="5"
+    stroke-width="3"
   />
   <circle
     cx="{size / 2}"
@@ -39,7 +48,7 @@
     r="{radius}"
     fill="none"
     stroke={color}
-    stroke-width="5"
+    stroke-width="3"
     stroke-dasharray="{circumference}"
     stroke-dashoffset="{strokeDashoffset}"
     transform={`rotate(-90 ${size / 2} ${size / 2})`}
@@ -49,7 +58,7 @@
     y="53%"
     dominant-baseline="middle"
     text-anchor="middle"
-    font-size="24"
+    font-size="0.95em"
     fill={color}
     font-family="'Inconsolata', sans-serif"
   >
